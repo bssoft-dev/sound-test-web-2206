06e4c0617e48bef1b4997d58795c1256a8eec68e
Sunghoon Cho <sunghoonism@gmail.com>
2023-05-22 14:55:31 +0900

    add layout props

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,9 +4,9 @@ import SideNav from "../SideNav/SideNav";
 import "./layout.css";
 
 export default function Layout(props) {
-    const {children, title} = props;
+    const {children, title, fetchData} = props;
     return (<div className="row layout">
         <SideNav />
-        <MainContainer content={children} title={title} />
+        <MainContainer content={children} title={title} fetchData={fetchData}  />
     </div>)
 }
