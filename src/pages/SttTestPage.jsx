84aa8e80363d680d53a3f5ed7fe2c1287bed096d
89aruc <89aruc@bs-soft.co.kr>
2023-06-13 14:25:44 +0900

    design: STT padding-bottom

--- src/pages/SttTestPage.jsx
+++ src/pages/SttTestPage.jsx
@@ -5,7 +5,7 @@ import { TimerContextProvider, TimerCtx } from "../context/TimerContext";
 import { RecordContextProvider, RecordCtx } from "../context/RecordContext";
 import RecordWaveSurfer from "../components/WaveSurferComp/RecordWaveSurfer";
 
-import { Grid, IconButton, Paper } from "@mui/material";
+import { Grid, IconButton, Paper, Typography } from "@mui/material";
 import SttRecord from "../components/SttRecord/SttRecord";
 import Loading from "../components/Loading/Loading";
 import AudioRecorder from "../components/AudioRecorder/AudioRecorder";
@@ -53,13 +53,13 @@ export default function SttTestPage() {
                 </Grid>
                 <Grid item>
                     {recordData && recordData.map((data, index) => {
-                        return (<p key={index}>{data}</p>)
+                        return (<Typography sx={{pb: 1}} key={index}>{data}</Typography>)
                     })}
                 </Grid>
-                <Grid item flex={1}>
+                {/* <Grid item flex={1}>
                     <embed src="https://sound.bs-soft.co.kr/receive/ws/byte"
                     width="100%" height="100%"></embed>
-                  </Grid>
+                </Grid> */}
                 </Grid>
             </Layout>
         </RecordContextProvider>
