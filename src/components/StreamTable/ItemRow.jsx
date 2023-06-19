4df22b0fd81d6a5247ad124795f9de3cc3303e2b
89aruc <89aruc@bs-soft.co.kr>
2023-06-19 12:53:53 +0900

    design: table spacing

--- src/components/StreamTable/ItemRow.jsx
+++ src/components/StreamTable/ItemRow.jsx
@@ -26,31 +26,31 @@ export default function ItemRow({historyRow}) {
                 </TableCell>
         </TableRow>
         <TableRow>
-            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-            <Collapse in={itemOpen} timeout="auto" unmountOnExit>
-                <Table size="small" aria-label="purchases">
-                    <TableHead>
-                        <StyledTableRow>
-                            <TableCell sx={{borderBottom: 'none'}}></TableCell>
-                            <StyledTableCell align="center">이벤트</StyledTableCell>
-                            <StyledTableCell align="center">파일이름</StyledTableCell>
-                            <StyledTableCell align="center">시작</StyledTableCell>
-                            <StyledTableCell align="center">끝</StyledTableCell>
-                        </StyledTableRow>
-                    </TableHead>
-                    <TableBody>
-                        {historyRow.itemData.map( (item, index) => (
-                            <TableRow key={index}>
+            <TableCell colSpan={6}>
+                <Collapse in={itemOpen} timeout="auto" unmountOnExit>
+                    <Table size="small" aria-label="purchases">
+                        <TableHead>
+                            <StyledTableRow>
                                 <TableCell sx={{borderBottom: 'none'}}></TableCell>
-                                <TableCell align="center">{item.event_label}</TableCell>
-                                <TableCell align="center">{String(item.filename)}</TableCell>
-                                <TableCell align="center">{item.onset}</TableCell>
-                                <TableCell align="center">{item.offset}</TableCell>
-                            </TableRow>
-                        ) )}
-                    </TableBody>
-                </Table>
-            </Collapse>
+                                <StyledTableCell align="center">이벤트</StyledTableCell>
+                                <StyledTableCell align="center">파일이름</StyledTableCell>
+                                <StyledTableCell align="center">시작</StyledTableCell>
+                                <StyledTableCell align="center">끝</StyledTableCell>
+                            </StyledTableRow>
+                        </TableHead>
+                        <TableBody>
+                            {historyRow.itemData.map( (item, index) => (
+                                <TableRow key={index}>
+                                    <TableCell sx={{borderBottom: 'none'}}></TableCell>
+                                    <TableCell align="center">{item.event_label}</TableCell>
+                                    <TableCell align="center">{String(item.filename)}</TableCell>
+                                    <TableCell align="center">{item.onset}</TableCell>
+                                    <TableCell align="center">{item.offset}</TableCell>
+                                </TableRow>
+                            ) )}
+                        </TableBody>
+                    </Table>
+                </Collapse>
             </TableCell>
         </TableRow>
         </>
