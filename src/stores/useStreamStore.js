be3535c8e360cd86b34337792c73d24923699e16
89aruc <89aruc@bs-soft.co.kr>
2024-01-17 13:46:52 +0900

    style: zustand subscribeWithSelector middleware delete

--- src/stores/useStreamStore.js
+++ src/stores/useStreamStore.js
@@ -1,77 +1,44 @@
-import { devtools, subscribeWithSelector } from "zustand/middleware";
+import { devtools } from "zustand/middleware";
 import { createWithEqualityFn } from "zustand/traditional";
 
 export const useStreamStore = createWithEqualityFn(
     devtools(
-        subscribeWithSelector(
-            (set) => ({
-                streamList: [],
-                setStreamList: (value) => set(
-                    { steamList: value }, false, 'setStreamList'
-                ),
-                handleStreamList: (value) => set(
-                    state => ({ steamList: [...state.setStreamList, value] }), false, 'handleStreamList'
-                ),
+        (set) => ({
+            streamList: [],
+            setStreamList: (value) => set(
+                { steamList: value }, false, 'setStreamList'
+            ),
+            handleStreamList: (value) => set(
+                state => ({ steamList: [...state.setStreamList, value] }), false, 'handleStreamList'
+            ),
 
-                audioSrc: null,
-                setAudioSrc: (value) => set(
-                    { audioSrc: value }, false, 'setAudioSrc'
-                ),
+            audioSrc: null,
+            setAudioSrc: (value) => set(
+                { audioSrc: value }, false, 'setAudioSrc'
+            ),
 
-                tempFile: null,
-                setTempFile: (value) => set(
-                    { tempFile: value }, false, 'setTempFile'
-                ),
+            tempFile: null,
+            setTempFile: (value) => set(
+                { tempFile: value }, false, 'setTempFile'
+            ),
 
-                status: '',
-                controlAudio: (value) => {
-                    set(
-                        {status: value}, false, 'controllAudio'
-                    );
-                    console.log("status", value);
-                },
-                
-                recordedData: null,
-                setRecordedData: (value) => set(
-                    { recordedData: value }, false, 'setRecordData'
-                ),
+            status: '',
+            controlAudio: (value) => {
+                set(
+                    {status: value}, false, 'controllAudio'
+                );
+                console.log("status", value);
+            },
+            
+            recordedData: null,
+            setRecordedData: (value) => set(
+                { recordedData: value }, false, 'setRecordData'
+            ),
 
-                rows: [],
-                setRows: (value) => set(
-                    state => ({ rows: [...state.rows, value] }), false, 'setRows'
-                ),
-            })
-        )
+            rows: [],
+            setRows: (value) => set(
+                state => ({ rows: [...state.rows, value] }), false, 'setRows'
+            ),
+        })
     ), Object.is
 )
-
-// useStreamStore.subscribe(
-//     ({ recordedData, streamList, setRows, setStreamList, setAudioSrc, setRecordedData }) => {
-//         console.log('recordedData', recordedData)
-//         const createData = (reckey, oriUrlBase, receivedTime, duration, history ) => {
-//             return {
-//               reckey,
-//               oriUrlBase,
-//               receivedTime,
-//               duration,
-//               history
-//             };
-//         }
-//         if (recordedData && streamList) {
-//             const newRow = createData(
-//             recordedData.Date,
-//             recordedData.blobURL,
-//             recordedData.Date,
-//             '10초',
-//             streamList,
-//             );
-//             console.log('streamList', streamList);
-//             setRows(newRow);
-//             setRecordedData(null);
-//             setStreamList([]);
-//             setAudioSrc('');
-//         }
-//     },
-    
-//     state => state.recordedData,
-// );
